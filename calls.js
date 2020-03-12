b56f9cc14f0febdde26f9a271b3ca8529f16d858
Joachim <joachim.ekstroem@gmail.com>
2020-03-12 16:06:34 +0100

    Added a procedure

--- calls.js
+++ calls.js
@@ -9,59 +9,16 @@ app.use(bodyParser.json())
 const router = express.Router()
 
 
-
-// Get books or add a new one
-router.route('/')
-    .get((req, res, next) => {     
-        pool((err, connection) => {
-            connection.query(`SELECT * FROM books`, (error, result, fields) => {
-                connection.release()
-                if (error) throw error
-                res.json(result)
-            })
+// Call procedure
+router.route('/procedure')
+.get((req, res, next) => {     
+    pool((err, connection) => {
+        connection.query(`CALL reset_loaned()`, (error, result, fields) => {
+            connection.release()
+            if (error) throw error
+            res.send("Procedure called - Reset books.loaned")
         })
     })
-    .post((req, res, next) => {     
-        pool((err, connection) => {
-            console.log(req.body)
-            connection.query(`INSERT INTO books (name, genre, loaned) VALUES ( ?, ?, 0)`, [req.body.name, req.body.genre], (error, result, fields) => {
-                connection.release()
-                if (error) throw error
-                res.send("Added a new book")
-            })
-        })
-    })
-
-// CRUD on one book
-router.route('/:id')
-
-    .get((req, res, next) => {     
-        pool((err, connection) => {
-            connection.query(`SELECT * FROM books WHERE id = ` + connection.escape(req.params.id), (error, result, fields) => {
-                connection.release()
-                if (error) throw error
-                res.json(result)
-            })
-        })
-    })
-    .put((req, res, post) => {
-        pool((err, connection) => {
-            connection.query(`UPDATE books SET name = ?, genre = ?, loaned = ?, WHERE books.id = ` + connection.escape(req.params.id), 
-            [req.body.name, req.body.genre, req.body.loaned], (error, result, fields) => {
-                connection.release()
-                if (error) throw error
-                res.send("Updated book with id: " + req.params.id + " with following values => Name: " + req.body.name + ", genre: " + req.body.genre + " loaned?: " + req.body.loaned)
-            })
-        })
-    })
-    .delete((req, res, next) => {
-        pool((err, connection) => {
-            connection.query(`DELETE FROM books WHERE id = ` + connection.escape(req.params.id), (error, result, fields) => {
-                connection.release()
-                if (error) throw error
-                res.send("Deleted book: " + req.params.id)
-            })
-        })
-    })
+})
 
 module.exports = router
